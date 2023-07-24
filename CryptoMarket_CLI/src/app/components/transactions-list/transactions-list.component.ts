bb6ebb17111796b51755d13f93db10678f15ca12
guerradiego2020 <guerradiego2020@gmail.com>
2023-07-24 18:58:32 -0300

    UF - transaction list changes

--- CryptoMarket_CLI/src/app/components/transactions-list/transactions-list.component.ts
+++ CryptoMarket_CLI/src/app/components/transactions-list/transactions-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Transaction } from 'src/app/models/transaction.model';
 import { DataService } from 'src/app/services/data-service.service';
 import { CommonService } from 'src/app/services/common.service';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-transactions-list',
@@ -13,14 +14,15 @@ export class TransactionsListComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.wallet = this.commonService.getWalletLinked();
     this.getTransactions();
     this.commonService.refreshData$.subscribe(() => {
       this.getTransactions();
-    })
+    });
   }
 
   transactions: Transaction[] = [];
-
+  wallet: string = "";
   showCount: number = 10;
 
   getTransactions(): void {
@@ -31,7 +33,7 @@ export class TransactionsListComponent implements OnInit {
         return;
       }
       
-      this.transactions = response.data;
+      this.transactions = response.data;debugger
 
     });
   }
